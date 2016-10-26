d46ba8a17e368a790fa513303f9174560b08b978
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-10-25 22:08:23 -0400

    Implemented use of local storage to save data

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -2,33 +2,36 @@
   angular.module('intro')
          .factory('TodoService', TodoService);
 
-  TodoService.$inject = [];
+  TodoService.$inject = ['$window'];
 
-  function TodoService(){
-    var data = [
-      { desc: 'take a nap' }, //each item is a Todo object instead of a Todo string
-      { desc: 'take another nap' },
-      { desc: 'get some milk' },
-      { desc: 'win the lotto' }
-    ];
-        return {
+  function TodoService($window){
+    var data = JSON.parse($window.localStorage.getItem('data'));
+
+    console.log($window.localStorage);
+
+    return {
       get: get,
       create: create,
       update: update,
       delete: remove,
     };
 
+
+
     function get(){
       return data;
     }
     function create(description){
       data.push({ desc: description });
+      $window.localStorage.setItem('data', JSON.stringify(data));
     }
     function update(index, newDescription){
       data.splice(index, 1, { desc: newDescription }); //modifies the data array
+      $window.localStorage.setItem('data', JSON.stringify(data));
     }
     function remove(index){
       data.splice(index, 1);
+      $window.localStorage.setItem('data', JSON.stringify(data));
     }
   }
 }());
